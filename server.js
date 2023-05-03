1d0f02b85eb8f340ff09d7904961544f4cb21577
Kavya Subramanian <kavyasub@unc.edu>
2023-05-03 15:25:44 -0400

    Endpoints added to server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//importing modules
+import { rps, rpsls } from 'lib/rpsls.js';
 //// Load most basic dependencies
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
@@ -81,6 +83,52 @@ if (args.debug) {
 } 
 // Log server start to file
 fs.appendFileSync(path.join(logpath, 'server.log'), startlog)
+
+//ENDPOINTS
+app.get('/app/', (req, res) => {
+    res.status(200).send('200 OK').end();
+});
+
+//Endpoint that return {"player: (rock|paper|scissors)"}
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+
+//Endpoint that return {"player":"(rock|paper|scissors|lizard|spock)"}
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+
+//
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
+})
+
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
+})
+
+//Default API endpoint that returns 404 NOT FOUND
+app.all('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
+})
+
 // Exit gracefully and log
 process.on('SIGINT', () => {
 // Create a log entry on SIGINT
